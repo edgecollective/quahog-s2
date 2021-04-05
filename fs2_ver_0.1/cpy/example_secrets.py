a88d16c55c09fde5c3ba5f1a3914c253abfd1e63
Don Blair <donblair@gmail.com>
2021-04-04 20:14:58 -0400

    update

--- fs2_ver_0.1/cpy/example_secrets.py
+++
@@ -1,7 +0,0 @@
-secrets = {
-    'ssid' : '[Wifi SSID]',
-    'password' : '[Wifi Password]',
-    'public_key' : '[Bayou Public Key]',
-    'private_key' : '[Bayou Private Key',
-    'base_url': 'http://192.168.1.163:5000/data/'
-    }
